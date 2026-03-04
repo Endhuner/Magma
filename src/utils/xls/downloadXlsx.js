2ffae54d40270cd8d579ae8cdabaddbdf222b6a0
Endhuner <daniel.vegvari@gmail.com>
2026-03-04 10:19:41 +0100

    Overwrite downloadXlsx.js with new content

--- src/utils/xls/downloadXlsx.js
+++ src/utils/xls/downloadXlsx.js
@@ -1,16 +1,32 @@
-// Implementation of xlsx-js-style
+// Using XLSX and Blob to write to a file
+function downloadXlsx(data, fileName) {
+    const ws = XLSX.utils.json_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
 
-import { writeFile } from 'xlsx-js-style';
+    const writeOptions = {
+        bookType: 'xlsx',
+        type: 'binary',
+    };
 
-export const downloadWorkbookXlsx = (workbook) => {
-    const wbout = writeFile(workbook, { bookType: 'xlsx', type: 'binary' });
-    const blob = new Blob([new Uint8Array(wbout)], { type: 'application/octet-stream' });
+    const wbout = XLSX.write(wb, writeOptions);
+    const blob = new Blob([s2ab(wbout)], {type: 'application/octet-stream'});
     const url = URL.createObjectURL(blob);
+
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'workbook.xlsx';
+    a.download = `${fileName}.xlsx`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
-};
+}
+
+function s2ab(s) {
+    const buf = new ArrayBuffer(s.length);
+    const view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) {
+        view[i] = s.charCodeAt(i) & 0xFF;
+    }
+    return buf;
+}
