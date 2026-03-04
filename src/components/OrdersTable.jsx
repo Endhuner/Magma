5459b73425bd7a52244f10c37464939d0be91824
Endhuner <daniel.vegvari@gmail.com>
2026-03-04 09:00:01 +0100

    Update OrdersTable.jsx to remove duplicated JSX block after the closing brace.

---
+++ src/components/OrdersTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrdersTable = () => {
+  // orders table implementation
+  return (
+    <div>
+      {/* Orders table content */}
+    </div>
+  );
+};
+
+export default OrdersTable;
